2829e401fe88607777a693a34de142b16fbeb485
itsbadr <68023752+cloudswritecode@users.noreply.github.com>
2021-04-06 15:15:59 +0500

    pattern loop @

--- Week 5/pattern.asm
+++ Week 5/pattern.asm
@@ -1,13 +1,119 @@
 .model small
 .stack 100h
 .data
-.code 
 
+first_row db 6 dup ('@')
+
+.code
 main proc
-
-    mov dx, 'a'
+    
+ 
+    mov ax, @data
+    mov ds, ax
+    
+    mov si, offset first_row  
+    mov cx, 6
+    
+    first_loop:
+        
+        mov dx, [si]
+        mov ah, 2
+        int 21h
+        
+        inc si
+        
+    loop first_loop
+    
+    mov dx, 10
     mov ah, 2
     int 21h
-
+    mov dx, 13
+    mov ah, 2
+    int 21h
+   
+    mov si, offset first_row
+    mov cx, 5
+    
+    second_loop:
+        
+        mov dx, [si]
+        mov ah, 2
+        int 21h
+        
+        inc si
+        
+    loop second_loop
+    
+    mov dx, 10
+    mov ah, 2
+    int 21h
+    mov dx, 13
+    mov ah, 2
+    int 21h
+    
+    mov si, offset first_row
+    mov cx, 4
+    
+    third_loop:
+        
+        mov dl, [si]
+        mov ah, 2
+        int 21h
+        
+        inc si
+        
+    loop third_loop
+    
+    mov dx, 10
+    mov ah, 2
+    int 21h
+    mov dx, 13
+    mov ah, 2
+    int 21h
+    
+    mov si, offset first_row
+    mov cx, 3
+    
+    fourth_loop:
+        
+        mov dx, [si]
+        mov ah, 2
+        int 21h
+        
+        inc si
+        
+    loop fourth_loop
+    
+    mov dx, 10
+    mov ah, 2
+    int 21h
+    mov dx, 13
+    mov ah, 2
+    int 21h
+    
+    mov si, offset first_row
+    mov cx, 2
+    
+    fifth_loop:
+        
+        mov dx, [si]
+        mov ah, 2
+        int 21h
+        
+        inc si
+        
+    loop fifth_loop
+    
+    mov dx, 10
+    mov ah, 2
+    int 21h
+    mov dx, 13
+    mov ah, 2
+    int 21h
+    
+    mov dx, '@'
+    mov ah, 2
+    int 21h
+   
     main endp
 end main
