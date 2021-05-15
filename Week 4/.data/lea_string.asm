c122b4d67dd4b81e9936a8f092a99a2c1f47fc3e
itsbadr <68023752+cloudswritecode@users.noreply.github.com>
2021-05-15 19:59:37 +0500

    data is moved to dl in lea

--- Week 4/.data/lea_string.asm
+++ Week 4/.data/lea_string.asm
@@ -9,7 +9,7 @@ main proc
     mov ax, @data                  
     mov ds, ax                     
     
-    lea dx, string         ; LEA: load effective address -> loads the entire string and puts it to dl
+    lea dx, string         ; LEA: load effective address -> loads the entire string and puts it to dx
     mov ah, 9
     int 21h
 
